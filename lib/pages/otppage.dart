68f160ae5ebd41beb91b67102e3e3474a8abc092
aswinning <aswinrox03@gmail.com>
2024-12-02 20:34:48 +0530

    w only

---
+++ lib/pages/otppage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class OtpPage extends StatelessWidget {
+  const OtpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/icons/image.png'), // Replace with your image
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
